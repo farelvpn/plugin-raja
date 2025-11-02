fb5c4e497292219ee51b7b65a81388ef1c5a099f
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-11-03 03:20:52 +0700

    Create add-vmess.sh

---
+++ add-vmess.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+domain=$(cat /etc/xray/domain)
+
+tls=443
+none=80
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+
+                read -rp "User: " -e user
+                CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+
+                if [[ ${CLIENT_EXISTS} -gt "0" ]]; then
+clear
+                        echo ""
+                        echo "A client with the specified name was already created, please choose another name."
+                        echo ""
+
+                        exit 0;
+                fi
+        done
+ 
+uuid=$(cat /proc/sys/kernel/random/uuid)
+#masa aktif akun 30 hari
+duration="30d"
+# Calculate expiration
+case "$duration" in
+    *m) seconds=$(( ${duration%m} * 60 )) ;;
+    *h) seconds=$(( ${duration%h} * 3600 )) ;;
+    *d) seconds=$(( ${duration%d} * 86400 )) ;;
+esac
+
+exp=$(date -d "+$seconds seconds" +"%Y-%m-%d-%H-%M-%S")
+exp_full=$exp
+
+quota="100"
+iplimit="5"
+
+# Set quota and IP limit
+if [[ $quota -gt 0 ]]; then
+    bytes=$((quota * 1024 * 1024 * 1024))
+    echo "$bytes" > "/etc/xray/limit/quota/vmess/$user"
+    quota_display="${quota} GB"
+else
+    quota_display="Unlimited"
+    bytes=0
+fi
+
+if [[ $iplimit -gt 0 ]]; then
+    echo "$iplimit" > "/etc/xray/limit/ip/vmess/$user"
+    iplimit_display="$iplimit"
+else
+    iplimit_display="Unlimited"
+fi
+
+# Add user to config
+sed -i '/#vmess$/a\### '"$user $exp_full"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
+
+# Save account database
+cat > /etc/xray/database/vmess/$user.txt <<EOF
+username: $user
+uuid: $uuid
+limit_ip: $iplimit
+quota: $quota
+expired: $exp_full
+EOF
+
+asu=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${user}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+ask=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "80",
+      "id": "${user}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF`
+grpc=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${user}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "vmess-grpc",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmess_base643=$( base64 -w 0 <<< $vmess_json3)
+vmesslink1="vmess://$(echo $asu | base64 -w 0)"
+vmesslink2="vmess://$(echo $ask | base64 -w 0)"
+vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
+systemctl restart xray > /dev/null 2>&1
+service cron restart > /dev/null 2>&1
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "     Xray/V2Ray/Vmess Account      " | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Domain : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port TLS : ${tls}" | tee -a /etc/log-create-user.log
+echo -e "Port none TLS : ${none}" | tee -a /etc/log-create-user.log
+echo -e "Port  GRPC : ${tls}" | tee -a /etc/log-create-user.log
+echo -e "id : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "alterId : 0" | tee -a /etc/log-create-user.log
+echo -e "Security : auto" | tee -a /etc/log-create-user.log
+echo -e "Network : ws" | tee -a /etc/log-create-user.log
+echo -e "Path : /vmess" | tee -a /etc/log-create-user.log
+echo -e "ServiceName : vmess-grpc" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Link TLS : ${vmesslink1}" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Link non-TLS : ${vmesslink2}" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC : vmess://soon" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
